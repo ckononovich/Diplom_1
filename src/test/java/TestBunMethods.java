0849e2adea6c4cace7f72a31a70c3bb2d02dec5f
Katerina Kononovich <kononovich-c@yandex.kz>
2024-08-19 10:56:30 +0300

    Change tests for Bun and Ingredient methods

--- src/test/java/TestBunMethods.java
+++ src/test/java/TestBunMethods.java
@@ -2,42 +2,47 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import praktikum.Bun;
+import praktikum.Database;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class TestBunMethods {
-
+    private final int bunIndex;
     private final String bunName;
     private final float bunPrice;
 
-    public TestBunMethods(String bunName, float bunPrice) {
+    public TestBunMethods(int bunIndex, String bunName, float bunPrice) {
+        this.bunIndex = bunIndex;
         this.bunName = bunName;
         this.bunPrice = bunPrice;
+
     }
 
     @Parameterized.Parameters
     public static Object[][] getBunNameAndPrice() {
         return new Object[][]{
-                {"black bun", 100},
-                {"white bun", 200},
-                {"red bun", 300},
+                {0, "black bun", 100},
+                {1, "white bun", 200},
+                {2, "red bun", 300},
         };
     }
 
     @Test
     public void bunNameIsDisplayed() {
+        Database database = new Database();
         Bun bun = new Bun(bunName, bunPrice);
         String actualResult = bun.getName();
         System.out.println(actualResult);
-        assertEquals(bun.getName(), actualResult);
+        assertEquals(database.availableBuns().get(bunIndex).getName(), actualResult);
     }
 
     @Test
     public void bunPriceIsDisplayed() {
+        Database database = new Database();
         Bun bun = new Bun(bunName, bunPrice);
         float actualResult = bun.getPrice();
         System.out.println(actualResult);
-        assertEquals(bun.getPrice(), actualResult, 2);
+        assertEquals(database.availableBuns().get(bunIndex).getPrice(), actualResult, 2);
     }
 }
--- src/test/java/TestIngredientMethods.java
+++ src/test/java/TestIngredientMethods.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import praktikum.Database;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
@@ -10,50 +11,54 @@ import static praktikum.IngredientType.SAUCE;
 
 @RunWith(Parameterized.class)
 public class TestIngredientMethods {
-
+    private final int index;
     private final IngredientType type;
     private final String name;
     private final float price;
 
-    public TestIngredientMethods(IngredientType type, String name, float price) {
+    public TestIngredientMethods(int index, IngredientType type, String name, float price) {
         this.type = type;
         this.name = name;
         this.price = price;
+        this.index = index;
     }
 
     @Parameterized.Parameters
     public static Object[][] getIngredientsData() {
         return new Object[][]{
-                {SAUCE, "hot sauce", 100},
-                {SAUCE, "sour cream", 200},
-                {SAUCE, "chili sauce", 300},
-                {FILLING, "cutlet", 100},
-                {FILLING, "dinosaur", 200},
-                {FILLING, "sausage", 300},
+                {0, SAUCE, "hot sauce", 100},
+                {1, SAUCE, "sour cream", 200},
+                {2, SAUCE, "chili sauce", 300},
+                {3, FILLING, "cutlet", 100},
+                {4, FILLING, "dinosaur", 200},
+                {5, FILLING, "sausage", 300},
         };
     }
 
     @Test
     public void testGetPriceIngredient() {
+        Database database = new Database();
         Ingredient ingredient = new Ingredient(type, name, price);
         float actualResult = ingredient.getPrice();
         System.out.println(actualResult);
-        assertEquals(ingredient.getPrice(), actualResult, 2);
+        assertEquals(database.availableIngredients().get(index).getPrice(), actualResult, 2);
     }
 
     @Test
     public void testGetNameIngredient() {
+        Database database = new Database();
         Ingredient ingredient = new Ingredient(type, name, price);
         String actualResult = ingredient.getName();
         System.out.println(actualResult);
-        assertEquals(ingredient.getName(), actualResult);
+        assertEquals(database.availableIngredients().get(index).getName(), actualResult);
     }
 
     @Test
     public void testGetTypeIngredient() {
+        Database database = new Database();
         Ingredient ingredient = new Ingredient(type, name, price);
         IngredientType actualResult = ingredient.getType();
         System.out.println(actualResult);
-        assertEquals(ingredient.getType(), actualResult);
+        assertEquals(database.availableIngredients().get(index).getType(), actualResult);
     }
 }
